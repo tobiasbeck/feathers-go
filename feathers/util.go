9642457bb478c7395571ec4b8d8d8980220d853e
Tobias Beck <tob.beck@icloud.com>
2022-06-25 11:25:54 +0200

    added once and unregister to event listener

--- feathers/util.go
+++ feathers/util.go
@@ -2,6 +2,8 @@ package feathers
 
 import (
 	"reflect"
+
+	"go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
 )
 
 func getField(v interface{}, field string) (interface{}, bool) {
@@ -22,30 +24,69 @@ func setField(v interface{}, field string, value interface{}) {
 	reflect.Indirect(r).FieldByName(field).Set(rValue)
 }
 
+type listenerEntry struct {
+	key      uuid.UUID
+	listener EventListener
+	once     bool
+}
+
 type EventListener = func(data interface{})
 
 type EventEmitter struct {
-	eventListeners map[string][]EventListener
+	eventListeners map[string][]listenerEntry
 }
 
 func (el *EventEmitter) Emit(event string, data interface{}) {
 	if listeners, ok := el.eventListeners[event]; ok {
+		nl := make([]listenerEntry, 0, len(listeners))
 		for _, listener := range listeners {
-			listener(data)
+			listener.listener(data)
+			if !listener.once {
+				nl = append(nl, listener)
+			}
 		}
+		el.eventListeners[event] = nl
 	}
 }
 
-func (el *EventEmitter) On(event string, listener EventListener) {
+type EventListenerUnregister = func() bool
+
+func (el *EventEmitter) On(event string, listener EventListener) EventListenerUnregister {
 	if _, ok := el.eventListeners[event]; !ok {
-		el.eventListeners[event] = make([]EventListener, 0)
+		el.eventListeners[event] = make([]listenerEntry, 0)
 	}
-	el.eventListeners[event] = append(el.eventListeners[event], listener)
+	id, _ := uuid.New()
+	listenerE := listenerEntry{
+		key:      id,
+		listener: listener,
+		once:     false,
+	}
+	el.eventListeners[event] = append(el.eventListeners[event], listenerE)
+	return func() bool {
+		for k, listener := range el.eventListeners[event] {
+			if uuid.Equal(listener.key, listenerE.key) {
+				el.eventListeners[event] = append(el.eventListeners[event][:k], el.eventListeners[event][k+1:]...)
+				return true
+			}
+		}
+		return false
+	}
+}
+
+func (el *EventEmitter) Once(event string, listener EventListener) {
+	if _, ok := el.eventListeners[event]; !ok {
+		el.eventListeners[event] = make([]listenerEntry, 0)
+	}
+	listenerE := listenerEntry{
+		listener: listener,
+		once:     true,
+	}
+	el.eventListeners[event] = append(el.eventListeners[event], listenerE)
 }
 
 func NewEventEmitter() *EventEmitter {
 	return &EventEmitter{
-		eventListeners: make(map[string][]EventListener),
+		eventListeners: make(map[string][]listenerEntry),
 	}
 }
 
